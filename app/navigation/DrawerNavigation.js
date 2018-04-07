855afe8c19c828e7bed9b763954267fdee741d23
Anoop Rajta <anoop.rajta@gmail.com>
2018-04-07 17:39:57 +0530

    drawer navigation fixed

--- app/navigation/DrawerNavigation.js
+++ app/navigation/DrawerNavigation.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Image,TouchableOpacity } from 'react-native';
 import { StackNavigator, DrawerNavigator } from 'react-navigation';
-import TestComponent from '../containers/TestComponent';
+import TestComponent from '../containers/home/TestComponent';
+import DrawerContainer from '../containers/home/DrawerContainer';
 
-
+const processOne = require('../assets/icons/4_burger_btn_onclick.png');
 // drawer stack
+
 const DrawerStack = DrawerNavigator({
   testComponent: { screen: TestComponent },
+}, {
+  gesturesEnabled: false,
+  contentComponent: DrawerContainer
 });
 
 const DrawerNav = StackNavigator({
   drawerStack: { screen: DrawerStack }
 }, {
-  headerMode: 'float',
+  headerMode: 'none',
   navigationOptions: ({ navigation }) => ({
-    headerStyle: { backgroundColor: 'green' },
-    title: 'Logged In to your app!',
-    headerLeft: <Text onPress={() => navigation.navigate('DrawerOpen')}>Menu</Text>
+    headerStyle: { backgroundColor: 'gray' },
+    // title: 'Logged In to your app!',
+    headerLeft: <TouchableOpacity style={{ paddingLeft: 5 }}
+                  onPress={() => {
+                    console.log(navigation)
+                      if (navigation.state.index === 0) {
+                          navigation.navigate('DrawerOpen');
+                      } else {
+                          navigation.navigate('DrawerClose');
+                      }
+                  }}
+                >
+                  <Image source={processOne} style={{ width: 30, height: 30 }} />
+                </TouchableOpacity>
   })
 });
 
