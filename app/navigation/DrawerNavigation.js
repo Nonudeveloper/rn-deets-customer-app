8de154c620e17f415fb614a9b39672f0d1d16e6d
Shivam <shivam160291@gmail.com>
2018-04-25 17:40:30 +0530

    MOB-258, Custom right to left transition added to drawer screens

--- app/navigation/DrawerNavigation.js
+++ app/navigation/DrawerNavigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Image, TouchableOpacity } from 'react-native';
+import { Text, Image, TouchableOpacity, Animated, Easing } from 'react-native';
 import { StackNavigator, DrawerNavigator } from 'react-navigation';
 import SelectVehileScreen from '../containers/appointment/vehicle/index';
 import DrawerContainer from '../containers/home/DrawerContainer';
@@ -13,6 +13,59 @@ import ReviewScreen from '../containers/appointment/review/index';
 
 
 const processOne = require('../assets/icons/4_burger_btn_onclick.png');
+
+/**Cunstom Transitions */
+const MyTransition = (toIndex, thisSceneIndex, height, width, scenes, position) => {
+  const translateX = position.interpolate({
+          inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
+          outputRange: [width, 0, 0]
+  });
+  const translateY = position.interpolate({
+          inputRange: [0, thisSceneIndex],
+          outputRange: [height, 0]
+  });
+
+  const slideFromRight = { transform: [{ translateX }] };
+  const slideFromBottom = { transform: [{ translateY }] };
+
+  const lastSceneIndex = scenes[scenes.length - 1].index;
+
+  // Test whether we're skipping back more than one screen
+  if (lastSceneIndex - toIndex > 1) {
+      // Do not transoform the screen being navigated to
+      if (scene.index === toIndex) return;
+      // Hide all screens in between
+      if (scene.index !== lastSceneIndex) return { opacity: 0 };
+      // Slide top screen down
+      return slideFromBottom;
+  }
+  return slideFromRight;
+};
+
+/**Transition Configurator */
+
+const TransitionConfiguration = () => {
+  return {
+        transitionSpec: {
+            duration: 550,
+            easing: Easing.out(Easing.poly(4)),
+            timing: Animated.timing,
+            useNativeDriver: true,
+        },
+      // Define scene interpolation, eq. custom transition
+         screenInterpolator: (sceneProps) => {
+            const { position, layout, scene, index, scenes } = sceneProps;
+            const toIndex = index;
+            const thisSceneIndex = scene.index;
+            const height = layout.initHeight;
+            const width = layout.initWidth;
+            return MyTransition(toIndex, thisSceneIndex, height, width, scenes, position); 
+        }
+    };
+};
+
+
+
 // drawer stack
 
 const drawer1 = StackNavigator({
@@ -26,7 +79,8 @@ const drawer1 = StackNavigator({
   reviewScreen: { screen: ReviewScreen },
   notesScreen: { screen: NotesScreen },
 }, {
-  headerMode: 'none'
+  headerMode: 'none',
+  transitionConfig: TransitionConfiguration
 });
 
 const DrawerStack = DrawerNavigator({
