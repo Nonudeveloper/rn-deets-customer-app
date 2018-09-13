d0c32a048107a841f50e60aadef5f71d6adc0813
Shivam <shivam160291@gmail.com>
2018-09-13 13:20:41 +0530

    hide list after item click

--- app/components/geoSearch/index.js
+++ app/components/geoSearch/index.js
@@ -9,20 +9,19 @@ import GeoCodeSearch from './GeoCodeSearch';
 import * as geoActions from '../../redux/geoCoding/geoActions';
 
 
-const mapDispatchToProps = (dispatch) => {
-    return {
+const mapDispatchToProps = (dispatch) => ({
         actions: bindActionCreators(geoActions, dispatch),
-        searchAddress: _.throttle(data => dispatch(geoActions.searchAddress(data)), 2000)
+        // searchAddress: data => {
+        //     dispatch(geoActions.searchAddress(data));
+        // }
+        searchAddress: _.throttle(data => dispatch(geoActions.searchAddress(data)), 1000)
         // searchAddress: _.debounce(data => dispatch(geoActions.searchAddress(data)), 2000)
         //https://jsbin.com/liwafuvupu/edit?js,output
-    };
-};
+    });
 
-const mapStateToProps = (state) => {
-    return {
+const mapStateToProps = (state) => ({
         features: state.Geo.features
-    };
-};
+    });
 
 export default connect(mapStateToProps, mapDispatchToProps)(GeoCodeSearch);
 
