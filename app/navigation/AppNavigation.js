e34c6016c6fac81ba3afb12eca69d8d1a53a884f
Shivam <shivam160291@gmail.com>
2018-09-20 16:55:19 +0530

    commenting chat screens

--- app/navigation/AppNavigation.js
+++ app/navigation/AppNavigation.js
@@ -3,21 +3,21 @@ import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import DrawerNav from './DrawerNavigation';
 import LoginNav from './LoginNavigation';
-import Login from '../components/chat/Login';
-import Users from '../components/chat/Users';
-import Chat from '../components/chat/Chat';
+// import Login from '../components/chat/Login';
+// import Users from '../components/chat/Users';
+// import Chat from '../components/chat/Chat';
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
   loginStack: { screen: LoginNav },
   drawerStack: { screen: DrawerNav },
-  login: { screen: Login },
-  users: { screen: Users }, 
-  chat: { screen: Chat }
+  // login: { screen: Login },
+  // users: { screen: Users }, 
+  // chat: { screen: Chat }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'login'
+  initialRouteName: 'loginStack'
 });
 
 export default PrimaryNav;
